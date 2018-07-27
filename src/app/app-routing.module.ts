0401e0028d87a373987d1d28b5b2d79e504b78c1
decster1488 <decster1488@gmail.com>
2018-07-27 16:12:29 +0300

    routing update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthModule } from './auth/auth.module';
+import { CrudPageModule } from './crud-page/crud-page.module';
+import { CrudUserModule } from './crud-user/crud-user.module';
+import {FinOperatingModule} from './fin-operating/fin-operating.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'author',
+    loadChildren: 'app/auth/auth.module#AuthModule'
+  },
+  {
+    path: 'crud-page',
+    loadChildren: 'app/crud-page/crud-page.module#CrudPageModule'
+  },
+  {
+    path: 'crud-user',
+    loadChildren: 'app/crud-user/crud-user.module#CrudUserModule'
+  },
+  {
+    path: 'fin-operation',
+    loadChildren: 'app/fin-operating/fin-operating#FinOperatingModule'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
