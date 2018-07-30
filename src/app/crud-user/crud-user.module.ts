440e33ec2bc1816805c63cba24f6027b825d120a
decster1488 <decster1488@gmail.com>
2018-07-30 09:28:58 +0300

    genrate add-user component

--- src/app/crud-user/crud-user.module.ts
+++ src/app/crud-user/crud-user.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { CrudUserRoutingModule } from './crud-user-routing.module';
 import { UserMainPageComponent } from './user-main-page/user-main-page.component';
 import { UserEditPageComponent } from './user-edit-page/user-edit-page.component';
+import { AddUserComponent } from './add-user/add-user.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CrudUserRoutingModule
   ],
-  declarations: [UserMainPageComponent, UserEditPageComponent]
+  declarations: [UserMainPageComponent, UserEditPageComponent, AddUserComponent]
 })
 export class CrudUserModule { }
