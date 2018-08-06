d1589211fdcf46010db89fea8b32eb7b029bd667
decster1488 <decster1488@gmail.com>
2018-08-06 14:29:27 +0300

    crud-page/main-page form adding & scss updating

--- src/app/crud-page/main-page/main-page.component.ts
+++ src/app/crud-page/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
+  addNewPageForm: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.addNewPageForm = new FormGroup({
+      'label': new FormControl(),
+      'parentMenu' : new FormControl(),
+      'url' : new FormControl(),
+      'activePage' : new FormControl()
+    });
+  }
+
+  onSubmit() {
+    console.log('ky');
   }
 
 }
