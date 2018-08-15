095415676692667a8e9b76a9450b6a2ebe15d92f
decster1488 <decster1488@gmail.com>
2018-08-15 15:33:46 +0300

    user table responsive & css fixed

---
+++ src/app/fin-operating/user-fin-operating/user-fin-operating.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-fin-operating',
+  templateUrl: './user-fin-operating.component.html',
+  styleUrls: ['./user-fin-operating.component.scss']
+})
+export class UserFinOperatingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
