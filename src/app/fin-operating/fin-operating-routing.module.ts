0fe7a9a27c65d8fb40357214b5b55da9bb005a50
decster1488 <decster1488@gmail.com>
2018-08-15 17:50:54 +0300

    add fin service & company fin operating update

--- src/app/fin-operating/fin-operating-routing.module.ts
+++ src/app/fin-operating/fin-operating-routing.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {EditComponent} from './edit/edit.component';
+import {UserFinOperatingComponent} from './user-fin-operating/user-fin-operating.component';
+import {CompanyFinOperatingComponent} from './company-fin-operating/company-fin-operating.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'edit',
+    redirectTo: 'company-operations',
     pathMatch: 'full'
   },
   {
     path: 'edit',
     component: EditComponent
-  }
+  },
+  {
+    path: 'empl-operations',
+    component: UserFinOperatingComponent
+  },
+  {
+    path: 'company-operations',
+    component: CompanyFinOperatingComponent
+  },
 ];
 
 @NgModule({
