2f7645c88fd6bb242ee8153700eb8cb8d1f2becb
decster1488 <decster1488@gmail.com>
2018-08-16 12:41:58 +0300

    fin company operation Add new transaction

--- src/app/fin-operating/company-fin-operating/company-fin-operating.component.ts
+++ src/app/fin-operating/company-fin-operating/company-fin-operating.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FinCompanyService} from '../../core/services/fin-company.service';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-company-fin-operating',
@@ -9,14 +10,47 @@ import {FinCompanyService} from '../../core/services/fin-company.service';
 export class CompanyFinOperatingComponent implements OnInit {
 
   public operationList;
+  public addNewBlock = false;
+  public newTransactForm: FormGroup;
+
 
   constructor( private finCompanyService: FinCompanyService) { }
 
   ngOnInit() {
+
+    this.getTransactionList();
+
+    this.newTransactForm = new FormGroup({
+      transaction: new FormControl('', [Validators.required]),
+      amount: new FormControl('', [Validators.required])
+    });
+  }
+
+  getTransactionList () {
     this.finCompanyService.paymantsList().subscribe( res => {
       this.operationList = res;
       console.log(this.operationList);
     });
   }
+
+
+  hideAddNew () {
+    this.addNewBlock = !this.addNewBlock;
+  }
+
+  onSubmin (tran) {
+    const operation = {
+      date: new Date(),
+      transactions: tran.transaction,
+      amount: tran.amount,
+    }
+    this.finCompanyService.addNewOperation(operation).subscribe(res => {
+      this.getTransactionList();
+      this.addNewBlock = !this.addNewBlock;
+    });
+    console.log(tran);
+
+  }
+
 
 }
