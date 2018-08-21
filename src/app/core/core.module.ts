12d894c54370c722128dde775de4f69ee1082456
decster1488 <decster1488@gmail.com>
2018-08-21 12:26:58 +0300

    fin company component update & adding fin company pipe

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,13 @@ import {NgModule, Optional, SkipSelf} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ApiService} from './api.service';
 import { UsersFilterPipe } from './pipes/users-filter.pipe';
+import { FinCompanyPipe } from './pipes/fin-company.pipe';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [UsersFilterPipe],
+  declarations: [UsersFilterPipe, FinCompanyPipe],
   providers: [
     ApiService
   ]
