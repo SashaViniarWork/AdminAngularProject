ab9440ef1fccac90d5f878bceae8ee94489ed5c0
decster1488 <nazar.mentukh@etiya.com>
2018-09-14 09:41:05 +0300

    user-main-page sorry I forgot what was changet

--- src/app/core/services/fin-company.service.ts
+++ src/app/core/services/fin-company.service.ts
@@ -7,7 +7,8 @@ export class FinCompanyService {
 
   public urls = {
     getlist: 'http://localhost:3000/payments/getAllPayments',
-    addnew: 'http://localhost:3000/payments/addpayment'
+    addnew: 'http://localhost:3000/payments/addpayment',
+    disactive: 'http://localhost:3000/payments/update/:id'
   }
 
   constructor(private http: HttpClient ) { }
@@ -18,6 +19,10 @@ export class FinCompanyService {
 
   addNewOperation (operation) {
     return this.http.post(this.urls.addnew, operation);
+  }
+
+  deactivateOperation ( operation ) {
+    return this.http.put(this.urls.disactive + '/' + operation._id, operation);
   }
 
 }
